b944406ef69032aab916aec5dfd503ebf46497fd
Rhaniel <rhanielmag.xavier@gmail.com>
2018-10-18 18:44:23 -0300

    Adicionada a geração dos histogramas

--- Homework-II/Dados/scriptHWII.R
+++ Homework-II/Dados/scriptHWII.R
@@ -1,4 +1,5 @@
 #Verificando se os pacotes necessários estão instalados, e instalando-os caso contrário
+source("util.R")
 
 required_packages <- c("e1071", "AppliedPredictiveModeling", "caret", "corrplot")
 installed_packages <- rownames(installed.packages())
@@ -14,12 +15,22 @@ library(caret)
 library(corrplot)
 data(solubility)
 
+#Criando uma pasta para salvar as figuras geradas
+figures_path = file.path(getwd(),"figures")
+
 notFP<-names(solTrainX)[!grepl("FP",names(solTrainX))]
 contPredTrain<-solTrainX[,notFP]+1
 contPredTest<-solTestX[,notFP]+1
+
+#Gerando os Histogramas para cada preditor antes de remover a skewness
+make_histograms(data=contPredTrain, main_path=figures_path, sub_path = "before")
+
 pp<-preProcess(contPredTrain,method="BoxCox")
 contPredTrain<-predict(pp,contPredTrain)
 contPredTest<-predict(pp,contPredTest)
 dataTrans<-rbind(contPredTrain,contPredTest)
 correlacao<-cor(dataTrans)
 corrplot(correlacao,method="square",type="lower",order="FPC")
+
+#Gerando os Histogramas para cada preditor depois de remover a skewness
+make_histograms(data=contPredTrain, main_path=main_path, sub_path = "after")
