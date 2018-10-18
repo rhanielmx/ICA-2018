5363eed9e14ddf7ce31f3e1697d25fea0c05f3e6
Rhaniel <rhanielmag.xavier@gmail.com>
2018-10-18 19:04:38 -0300

    Atualizando a geração dos histogramas

--- Homework-II/Dados/scriptHWII.R
+++ Homework-II/Dados/scriptHWII.R
@@ -1,7 +1,8 @@
-#Verificando se os pacotes necessários estão instalados, e instalando-os caso contrário
-source("util.R")
+source("utils.R")
 
-required_packages <- c("e1071", "AppliedPredictiveModeling", "caret", "corrplot")
+#Verificando se os pacotes necessários estão instalados, e instalando-os caso contrário
+
+required_packages <- c("e1071", "AppliedPredictiveModeling", "caret", "corrplot", "ggplot2")
 installed_packages <- rownames(installed.packages())
 for (requirement in required_packages){
   if(!(requirement %in% installed_packages)){
@@ -13,17 +14,19 @@ library(e1071)
 library(AppliedPredictiveModeling)
 library(caret)
 library(corrplot)
+library(ggplot2)
 data(solubility)
 
 #Criando uma pasta para salvar as figuras geradas
 figures_path = file.path(getwd(),"figures")
+
 
 notFP<-names(solTrainX)[!grepl("FP",names(solTrainX))]
 contPredTrain<-solTrainX[,notFP]+1
 contPredTest<-solTestX[,notFP]+1
 
 #Gerando os Histogramas para cada preditor antes de remover a skewness
-make_histograms(data=contPredTrain, main_path=figures_path, sub_path = "before")
+make_histograms(data=contPredTrain, file_path=file.path(figures_path, "before"))
 
 pp<-preProcess(contPredTrain,method="BoxCox")
 contPredTrain<-predict(pp,contPredTrain)
@@ -33,4 +36,4 @@ correlacao<-cor(dataTrans)
 corrplot(correlacao,method="square",type="lower",order="FPC")
 
 #Gerando os Histogramas para cada preditor depois de remover a skewness
-make_histograms(data=contPredTrain, main_path=main_path, sub_path = "after")
+make_histograms(data=contPredTrain, file_path=file.path(figures_path, "after"))
