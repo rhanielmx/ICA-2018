89d9c55cc43d3b554104132bb68d8a834107dfcb
GTi <gtiengenhariajr@gmail.com>
2018-11-20 20:30:21 -0300

    SVM TUNADO

--- Homework-III/scripts/SVM.R
+++ Homework-III/scripts/SVM.R
@@ -1,8 +1,8 @@
 library(e1071)
 
-#AJEITAR O DIRETÓRIO PRA CARREGAR DIREITIN
-#load("C:/Users/Marcus Vinicius/Downloads/TI0077_HW3_assignment/grantData_hw3/grantData_HW3.RData")
-#reducedSet <- read.csv(file="C:/Users/Marcus Vinicius/Downloads/TI0077_HW3_assignment/grantData_hw3/reducedSet.csv", header=TRUE)
+#AJEITAR O DIRET?RIO PRA CARREGAR DIREITIN
+load("/home/marcusv/HW/ICA-2018/Homework-III/dados/grantData_hw3/grantData_HW3.RData")
+reducedSet <- read.csv(file="/home/marcusv/HW/ICA-2018/Homework-III/dados/grantData_hw3/reducedSet.csv", header=TRUE)
 
 #Reduzindo os dados apenas para os preditores que interessam e a coluna de Resultados
 dados_reduzidos <- testing[c(t(reducedSet), 'Class')]
@@ -16,5 +16,21 @@ no_class <- testing[ ,t(reducedSet)]
 #Predizemos utilizando o modelo desenvolvido com os preditores
 result <- predict(svm_modelo, no_class)
 
-#Matriz de confusão com nossos resultados de predição e com os resultados que já temos
+#Matriz de confus?o com nossos resultados de predi??o e com os resultados que j? temos
 table(result, dados_reduzidos$Class)
+
+#TUNANDO O MODELO
+#Tunando utilizando o kernel radial e especificando uma lista de valores Gamma e C
+tune_svm <- tune.svm(Class ~ ., data=dados_reduzidos, kernel="radial", gamma = 10^(-1:-3), cost = 10^(1:3), scale = FALSE)
+
+#Vemos os valores Gamma e C que geram melhor resultado
+summary(tune_svm)
+
+#Elaboramos um Modelo utilizando os melhores valores de C e Gamma
+svm_modelo_tunado <- svm(Class ~ ., data=dados_reduzidos, kernel="radial", cost=100, gamma=0.001, scale=FALSE)
+
+#Predizemos os valores usando o modelo tunado
+result_tunado <- predict(svm_modelo_tunado, no_class)
+
+#Matriz de confusÃ£o com os nossos resultados de prediÃ§Ã£o e com os resultados que jÃ¡ temos
+table(result_tunado, dados_reduzidos$Class)
